1acf48b4a340b44e62e282a4290727c2cb0b9ff7
Prabigya Acharya <acharyaprabigya@gmail.com>
2022-11-20 16:28:58 +0545

    api fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
@@ -7,6 +7,7 @@ import sklearn.externals
 import joblib
 import pickle
 from collections.abc import Mapping
+
 
 # load the model from disk
 filename = 'nlp_model.pkl'
@@ -20,14 +21,25 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        message = request.form['message']
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-    return render_template('result.html', prediction=my_prediction)
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         message = request.form['message']
+#         data = [message]
+#         vect = cv.transform(data).toarray()
+#         my_prediction = clf.predict(vect)
+#     return render_template('result.html', prediction=my_prediction)
+
+
+@app.route('/predict/<text>', methods=['GET'])
+def recommend(text):
+    data = [text]
+    vect = cv.transform(data).toarray()
+    my_prediction = clf.predict(vect)
+    if my_prediction == 1:
+        return jsonify({'prediction': "Spam"})
+    else:
+        return jsonify({'prediction': "Not Spam"})
 
 
 if __name__ == '__main__':
